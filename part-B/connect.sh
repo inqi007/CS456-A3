3bb13c8d77d68e4d90ac71652c323d393a776ad1
Inqiyad <inqiyad18@gmail.com>
2023-07-25 16:47:39 -0400

    partb

--- part-B/connect.sh
+++ part-B/connect.sh
@@ -29,24 +29,30 @@ ofctl='ovs-ofctl -O OpenFlow13'
 
 # OVS rules for s1
 $ofctl add-flow s1 \
-   dl_src=0A:00:0A:01:00:02, dl_dst=0A:00:04:01:00:01, action=output:2
+    in_port=1,ip,nw_src=10.1.1.17,nw_dst=10.4.4.48,actions=mod_dl_src:0A:00:0A:01:00:02,mod_dl_dst:0A:00:04:01:00:01,output=2
+#    dl_src=0A:00:0A:01:00:02, dl_dst=0A:00:04:01:00:01, action=output:2
 
 $ofctl add-flow s1 \
-   dl_src=0A:00:00:01:00:01, dl_dst=aa:aa:aa:aa:aa:aa, actions=output:1
+    in_port=2,ip,nw_src=10.4.4.48,nw_dst=10.1.1.17,actions=mod_dl_src:0A:00:00:01:00:01,mod_dl_dst:aa:aa:aa:aa:aa:aa,output=1
+#    dl_src=0A:00:00:01:00:01, dl_dst=aa:aa:aa:aa:aa:aa, actions=output:1
 
 # OVS rules for r1
 $ofctl add-flow r1 \
-    dl_src=0A:00:0E:FE:00:02, dl_dst=0A:00:01:01:00:01, action=output:2
+    in_port=1,ip,nw_src=10.1.1.17,nw_dst=10.4.4.48,actions=mod_dl_src:0A:00:0E:FE:00:02,mod_dl_dst:0A:00:01:01:00:01,output=2
+    # dl_src=0A:00:0E:FE:00:02, dl_dst=0A:00:01:01:00:01, action=output:2
 
 $ofctl add-flow r1 \
-    dl_src=0A:00:04:01:00:01, dl_dst=0A:00:0A:01:00:02, actions=output:1
+    in_port=2,ip,nw_src=10.4.4.48,nw_dst=10.1.1.17,actions=mod_dl_src:0A:00:04:01:00:01,mod_dl_dst:0A:00:0A:01:00:02,output=1
+    # dl_src=0A:00:04:01:00:01, dl_dst=0A:00:0A:01:00:02, actions=output:1
 
 # OVS rules for s2
 $ofctl add-flow s2 \
-    dl_src=0A:00:0A:FE:00:02, dl_dst=b0:b0:b0:b0:b0:b0, action=output:2
+    in_port=1,ip,nw_src=10.1.1.17,nw_dst=10.4.4.48,actions=mod_dl_src:0A:00:0A:FE:00:02,mod_dl_dst:b0:b0:b0:b0:b0:b0,output=2
+    # dl_src=0A:00:0A:FE:00:02, dl_dst=b0:b0:b0:b0:b0:b0, action=output:2
 
 $ofctl add-flow s2 \
-    dl_src=0A:00:01:01:00:01, dl_dst=0A:00:0E:FE:00:02, actions=output:1
+    in_port=2,ip,nw_src=10.4.4.48,nw_dst=10.1.1.17,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:0E:FE:00:02,output=1
+    # dl_src=0A:00:01:01:00:01, dl_dst=0A:00:0E:FE:00:02, actions=output:1
 
 
 # Print the flows installed in each switch
